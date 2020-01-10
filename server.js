932a2e2f09307947466f4a6f97ee67e735083871
Faran42 <faranorphys@gmail.com>
2020-01-10 13:44:54 -0300

    CSS organizado, criar marcacao de exames

--- server.js
+++ server.js
@@ -11,22 +11,30 @@ const pacienteController = require('./controllers/pacienteController');
 const indexController = require('./controllers/indexController');
 
 const app = express()
+
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.use(express.static('views')) //aponta o caminho da pasta views e por consequencia o css dela
 app.set('views engine', 'hbs');
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+
+
 
 app.listen(3000, function(){
     console.log('Listen to Port 3000')
 })
+
 app.get('/', (req, res) => {
     res.render('index/index.hbs', {
         viewTitle : "Home" 
     })
 })
+
+// chamada de controles
 app.use('/user', userController);
 app.use('/exame', exameController);
 app.use('/paciente', pacienteController);
