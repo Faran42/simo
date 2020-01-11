44e512f925ced2a32323c682bfd3ef935c190957
Faran42 <faranorphys@gmail.com>
2020-01-10 23:13:23 -0300

    Aprimoramento no modal da index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 const mongoose = require('mongoose');
 const Paciente = mongoose.model('Paciente');
+const Exame = mongoose.model('Exame');
 
 router.get('/paciente/:nome', (req, res) => {
     var q = Paciente.find({nome : new RegExp(req.params.nome, 'i')}).limit(8);
     q.exec(function(err, paciente) {
         res.json(paciente);
+    });
+});
+
+router.get('/exame/:nome', (req, res) => {
+    var q = Exame.find({nome : new RegExp(req.params.nome, 'i')}).limit(8);
+    q.exec(function(err, exame) {
+        res.json(exame);
     });
 });
 
